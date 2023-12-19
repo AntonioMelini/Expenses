cae9c847a23a5c7a930ac0cb91605db0253cdce6
AntonioMelini <antoniomelini@hotmail.com>
2023-12-19 10:37:29 -0300

    development

--- src/main/java/com/project/ExpenseApplication.java
+++ src/main/java/com/project/ExpenseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExpenseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExpenseApplication.class, args);
+		SpringApplication.run(ExpenseApplication.class, args);  
 	}
 
 }
