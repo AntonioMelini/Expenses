dec3d311b7599372c410306e0d235d8bd46dfa0a
AntonioMelini <antoniomelini@hotmail.com>
2023-12-19 11:48:27 -0300

    adding ExpenseCategory entity

---
+++ src/main/java/com/project/entity/Expense.java
@@ -0,0 +1,62 @@
+package com.project.entity;
+
+import java.time.LocalDateTime;
+
+public class Expense {
+    public Expense() {
+    }
+
+    public Expense(Long id, String description, Double amount, LocalDateTime date) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    private Long id;
+    private String description;
+    private Double amount;
+    private LocalDateTime date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", date=" + date +
+                '}';
+    }
+}
