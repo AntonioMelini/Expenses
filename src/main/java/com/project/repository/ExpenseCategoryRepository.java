35b7f69ae687005c2c2b74afa5f1a9692b6bc225
AntonioMelini <antoniomelini@hotmail.com>
2023-12-20 15:09:14 -0300

    adding getALlExpenseCategory

--- src/main/java/com/project/repository/ExpenseCategoryRepository.java
+++ src/main/java/com/project/repository/ExpenseCategoryRepository.java
@@ -1,7 +1,12 @@
 package com.project.repository;
+import com.project.dto.request.ExpenseCategoryRequestDto;
 import com.project.entity.ExpenseCategory;
+
+import java.util.List;
 
 public interface ExpenseCategoryRepository {
     Integer insert(ExpenseCategory expenseCategory);
     void getCategoryByName(String name);
+
+    List<ExpenseCategoryRequestDto> getAll();
 }
--- src/main/java/com/project/service/ExpenseCategoryService.java
+++ src/main/java/com/project/service/ExpenseCategoryService.java
@@ -2,6 +2,10 @@ package com.project.service;
 
 import com.project.dto.request.ExpenseCategoryRequestDto;
 
+import java.util.List;
+
 public interface ExpenseCategoryService {
     String insert(ExpenseCategoryRequestDto expenseCategoryRequestDto);
+
+    List<ExpenseCategoryRequestDto> getAll();
 }
