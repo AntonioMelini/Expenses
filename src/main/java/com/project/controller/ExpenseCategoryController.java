3aa304d35cab1f539a09b3c52c7bfb37f3c5d8d2
AntonioMelini <antoniomelini@hotmail.com>
2023-12-22 09:56:54 -0300

    adding ResponseEntity to controllers

--- src/main/java/com/project/controller/ExpenseCategoryController.java
+++ src/main/java/com/project/controller/ExpenseCategoryController.java
@@ -4,6 +4,7 @@ import com.project.dto.request.ExpenseCategoryRequestDto;
 import com.project.entity.ExpenseCategory;
 import com.project.service.ExpenseCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,18 +20,24 @@ public class ExpenseCategoryController {
     }
 
     @PostMapping()
-    public String insertExpenseCategory(@RequestBody ExpenseCategoryRequestDto expenseCategoryRequestDto){
-        return expenseCategoryService.insert(expenseCategoryRequestDto);
+    public ResponseEntity<String> insertExpenseCategory(@RequestBody ExpenseCategoryRequestDto expenseCategoryRequestDto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(expenseCategoryService.insert(expenseCategoryRequestDto));
 
     }
     @GetMapping()
-    public List<ExpenseCategoryRequestDto> getAllExpenseCategory(){
-        return expenseCategoryService.getAll();
+    public ResponseEntity<List<ExpenseCategoryRequestDto>> getAllExpenseCategory(){
+        return  ResponseEntity
+                .status(HttpStatus.OK)
+                .body(expenseCategoryService.getAll());
     }
 
 
     @GetMapping("/{id}")
-    public  ExpenseCategoryRequestDto getExpenseCategoryById(@PathVariable int id){
-        return expenseCategoryService.getById(id);
+    public  ResponseEntity<ExpenseCategoryRequestDto> getExpenseCategoryById(@PathVariable int id){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(expenseCategoryService.getById(id));
     }
 }
